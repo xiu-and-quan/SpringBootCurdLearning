190f96246907eee55cd82e6f33c127e56680f87f
shizq18 <shizq18@meicloud.com>
2022-09-07 11:08:55 +0800

    新建项目curd_02

---
+++ curd_02/src/main/java/com/example/curd_02/Curd02Application.java
@@ -0,0 +1,13 @@
+package com.example.curd_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curd02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Curd02Application.class, args);
+    }
+
+}
---
+++ curd_02/src/test/java/com/example/curd_02/Curd02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.curd_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Curd02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
